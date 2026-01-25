f230f10ed8b221eb770637d0454d6bcd95e9573c
YelzyDy <zendymariel.dy@gmail.com>
2026-01-25 18:46:48 +0800

    Revert "basic movement animation"
    This reverts commit 6a4ff3e40ebad70fc87e2717d48f14937caff93a.

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -1,21 +1,10 @@
 extends CharacterBody2D
 
-<<<<<<< Updated upstream
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-=======
-const SPEED := 300.0
-const JUMP_VELOCITY := -400.0
-
-@onready var sprite: Sprite2D = $Sprite2D
-@onready var anim: AnimationPlayer = $AnimationPlayer
-
-var was_on_floor := false
->>>>>>> Stashed changes
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-<<<<<<< Updated upstream
 func _physics_process(delta: float) -> void:
 	# Apply gravity
 	if not is_on_floor():
@@ -23,25 +12,13 @@ func _physics_process(delta: float) -> void:
 
 	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-=======
-func _physics_process(delta):
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
->>>>>>> Stashed changes
 		velocity.y = JUMP_VELOCITY
 		sprite.play("jump")
 
-<<<<<<< Updated upstream
 	# Horizontal movement
 	var direction := Input.get_axis("left", "right")
-=======
-	var direction := Input.get_axis("ui_left", "ui_right")
->>>>>>> Stashed changes
 	if direction != 0:
 		velocity.x = direction * SPEED
-		sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
@@ -59,27 +36,3 @@ func _physics_process(delta):
 			sprite.play("default")  # idle animation
 
 	move_and_slide()
-
-	update_animation(direction)
-
-	was_on_floor = is_on_floor()
-
-
-func update_animation(direction):
-	if not is_on_floor():
-		if velocity.y < 0:
-			safe_play("jump")
-		else:
-			safe_play("fall")
-		return
-
-
-	if direction != 0:
-		safe_play("walk")
-	else:
-		safe_play("idle")
-
-
-func safe_play(name: String):
-	if anim.current_animation != name:
-		anim.play(name)
