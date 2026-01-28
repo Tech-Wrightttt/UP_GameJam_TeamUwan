a2b9ab1f84d15b0bbf1239195950f168a2708341
xienshane <onglovelyshane71@gmail.com>
2026-01-29 03:31:53 +0800

    bad ending voice

--- scripts/bad_ending.gd
+++ scripts/bad_ending.gd
@@ -8,6 +8,7 @@ extends Control
 @onready var anim_player2 = $CanvasLayer2/AnimationPlayer
 @onready var anim_player3 = $CanvasLayer3/AnimationPlayer
 
+@onready var audio_player = $audio
 
 @onready var sprite = $CanvasLayer/AnimatedSprite2D
 @onready var sprite2 = $CanvasLayer2/AnimatedSprite2D
@@ -19,29 +20,25 @@ func _ready() -> void:
 	layer2.hide()
 	layer3.hide()
 	
+	if audio_player:
+		audio_player.play()
+	
 	sprite.play("hurt")
 	sprite2.play("hurt")
 	sprite3.play("hurt")
 	
-	await play_layer_sequence(layer1, anim_player1)
-	await play_layer_sequence(layer2, anim_player2)
-	await play_layer_sequence(layer3, anim_player3)
+	await play_layer_sequence(layer1, anim_player1, 9.5)  
+	await play_layer_sequence(layer2, anim_player2, 10) 
+	await play_layer_sequence(layer3, anim_player3, 15) 
 	
 	get_tree().change_scene_to_file("res://levels/title_screen.tscn")
 
-func play_layer_sequence(layer: CanvasLayer, anim: AnimationPlayer) -> void:
+func play_layer_sequence(layer: CanvasLayer, anim: AnimationPlayer, animation_length: float) -> void:
 	layer.show()
 	
-	## Start from black, then fade to show the text
-	#anim.play("fade_in") 
-	# Play typewriter immediately or shortly after
 	anim.queue("typewriter") 
 	
-	# Wait for the player to read (7 seconds)
-	await get_tree().create_timer(7.0).timeout
-	
-	## Fade back to black
-	#anim.play("fade_out")
-	#await anim.animation_finished
+	await get_tree().create_timer(animation_length).timeout
+
 	
 	layer.hide()
