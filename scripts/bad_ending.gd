5a480e60b0f5d326b06706f5ffab770cacf258d6
xienshane <onglovelyshane71@gmail.com>
2026-01-29 22:32:01 +0800

    fixed parallax

--- scripts/bad_ending.gd
+++ scripts/bad_ending.gd
@@ -31,7 +31,7 @@ func _ready() -> void:
 	await play_layer_sequence(layer2, anim_player2, 10) 
 	await play_layer_sequence(layer3, anim_player3, 15) 
 	
-	get_tree().change_scene_to_file("res://levels/title_screen.tscn")
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func play_layer_sequence(layer: CanvasLayer, anim: AnimationPlayer, animation_length: float) -> void:
 	layer.show()
