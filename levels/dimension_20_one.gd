d2f722a10b1e83feb55cb1419e36bfa83a0baafe
Ruhmer Jairus Espina <ruhmerjairus.espina@cit.edu>
2026-01-30 14:53:51 +0800

    DARK DIMENSION TRANSITIONS

---
+++ levels/dimension_20_one.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_towardso_1_body_entered(body: Node2D) -> void:
+	if body.name == "main_character":
+		GameManager.fade_out(get_tree().current_scene,"res://levels/dimension20_two.tscn", 0.8,Color.BLACK)
