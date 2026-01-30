f3416b61a43e9cba5aedef67d91142f156cf64e4
Ruhmer Jairus Espina <ruhmerjairus.espina@cit.edu>
2026-01-30 14:37:25 +0800

    FIXED TRANSITIONS SA CASTLE

--- scripts/castle_three.gd
+++ scripts/castle_three.gd
@@ -5,6 +5,7 @@ extends Node2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var sprite1: AnimatedSprite2D = $AnimatedSprite2D2
 func _ready() -> void:
+	$main_character.global_position = GameManager.spawn_points[3]
 	sprite.play("default")
 	sprite1.play("default")
 
@@ -12,3 +13,24 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_towardso_1_body_entered(body: Node2D) -> void:
+	if body.name == "main_character":
+		GameManager.fade_out(get_tree().current_scene,"res://levels/castle_two.tscn", 0.8,Color.BLACK)
+
+
+
+
+func _on_towardsc_1_body_entered(body: Node2D) -> void:
+	if body.name == "main_character":
+		GameManager.fade_out(get_tree().current_scene,"res://levels/castle_bossroom.tscn",0.8,Color.BLACK)
+
+
+var player
+
+func _on_marker_body_entered(body: Node2D) -> void:
+	if body.name == "main_character":
+		player = body  # Store the Node2D itself, not body.name
+		if player and is_instance_valid(player):
+			GameManager.spawn_points[3] = player.global_position
--- scripts/castle_two.gd
+++ scripts/castle_two.gd
@@ -5,6 +5,7 @@ const TOTAL_CLOCKS := 3
 @onready var counter_label: Label = $counter
 
 func _ready():
+	$main_character.global_position = GameManager.spawn_points[2]
 	#update_counter()
 	print("Level Started")
 
@@ -14,3 +15,29 @@ func add_clock():
 
 #func update_counter():
 	#counter_label.text = str(GameManager.current_clocks) + "/" + str(TOTAL_CLOCKS)
+
+
+func _on_towardso_1_body_entered(body: Node2D) -> void:
+	if body.name == "main_character":
+		GameManager.fade_out(get_tree().current_scene,"res://levels/castle_one.tscn", 0.8,Color.BLACK)
+
+
+
+
+func _on_towardsc_1_body_entered(body: Node2D) -> void:
+	if body.name == "main_character":
+		GameManager.fade_out(get_tree().current_scene,"res://levels/castle_three.tscn",0.8,Color.BLACK)
+
+
+var player
+
+func _on_marker_body_entered(body: Node2D) -> void:
+	if body.name == "main_character":
+		player = body  # Store the Node2D itself, not body.name
+		if player and is_instance_valid(player):
+			GameManager.spawn_points[2] = player.global_position
+
+
+
+func _on_towards_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
